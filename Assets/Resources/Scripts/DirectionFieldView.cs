f31003279add7fef18bdb20bf8e4319314d55d8c
ent1k1337@yandex.ru <ent1k1337@yandex.ru>
2022-05-09 13:26:14 +0300

    added animation when opening/closing direction field

--- Assets/Resources/Scripts/DirectionFieldView.cs
+++ Assets/Resources/Scripts/DirectionFieldView.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,10 +12,21 @@ namespace Resources.Scripts
         [SerializeField] private List<Sprite> _directionIcons;
         [SerializeField] private List<DirectionIcon> _directions;
         [SerializeField] private GameObject _directionField;
-        
-        public void Activate() => _directionField.SetActive(true);
 
-        public void Deactivate() => _directionField.SetActive(false);
+        private readonly Vector3 _minScaleDirectionField = new(0.1f, 0.1f, 0.1f);
+        private readonly float _durationScale = 0.15f;
+
+        public void Activate()
+        {
+            _directionField.transform.localScale = _minScaleDirectionField;
+            _directionField.SetActive(true);
+            _directionField.transform.DOScale(new Vector3(1, 1, 1), _durationScale);
+        }
+
+        public void Deactivate()
+        {
+            _directionField.transform.DOScale(_minScaleDirectionField, _durationScale).OnComplete(() => _directionField.SetActive(false));
+        }
 
         public void SetCurrentActiveDirection(int index)
         {
